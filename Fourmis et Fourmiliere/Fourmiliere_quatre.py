d8b3420d00704c5a0486317cd82d8b3b035e14ee
imane-ayouni <81591745+imane-ayouni@users.noreply.github.com>
2021-10-25 17:35:31 +0200

    Add files via upload

---
+++ Fourmis et Fourmiliere/Fourmiliere_quatre.py
@@ -0,0 +1,70 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
+class Vestibule:
+    def __init__(self,name, size):
+        self.name = name
+        self.size = size
+
+class Salles:
+    def __init__(self,name,size):
+        self.name = name
+        self.size = size
+
+class Dorm:
+    def __init__(self,name,size):
+        self.name = name
+        self.size = size
+size = 10
+
+Sv = Vestibule("Sv",size)
+salles = Salles(("S1","S2","S3","S4","S5","S6"),(0,0,0,0,0,0))
+dorm = Dorm("Sd",0)
+print("There are ", size, " ants")
+
+
+
+class Anthill:
+    def __init__(self):
+        self.nodes = []
+        self.edges = []
+    def addNodes(self,a,b,c,d):
+        nodes = [a,b,c,d]
+        self.nodes.append(nodes)
+    def addEdges(self,a,b):
+        edges = [a,b]
+        self.edges.append(edges)
+    def visualizeGraph(self):
+        graph = nx.Graph()
+        graph.add_nodes_from(self.nodes)
+        graph.add_edges_from(self.edges)
+        nx.draw_networkx(graph)
+        plt.show()
+
+graph = Anthill()
+graph.addEdges(salles.name[2],salles.name[3])
+graph.addEdges(Sv.name,salles.name[0])
+graph.addEdges(salles.name[0],salles.name[1])
+graph.addEdges(salles.name[1],salles.name[3])
+graph.addEdges(salles.name[3],salles.name[4])
+graph.addEdges(salles.name[4],dorm.name)
+graph.addEdges(salles.name[3],salles.name[5])
+graph.addEdges(salles.name[5],dorm.name)
+graph.addEdges(salles.name[0],salles.name[2])
+
+graph.visualizeGraph()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
